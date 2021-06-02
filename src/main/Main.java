7c2c80ce2503b70e672d382f259f96b75bec1c15
Pablo Zulategui <pablillozc@gmail.com>
2021-06-02 17:37:03 +0200

    Arreglar fallos del merge

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,12 +5,7 @@ import vistas.Login;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		new Login();
-		
-=======
-    System.out.println("Hola mundo");
->>>>>>> 15bfe8ba2d92b969205d4c06df1358e092398dce
 	}
 
 }
